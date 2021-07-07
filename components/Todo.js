7fd15c4436d02c97f3e4e6d633ec8a1e9d4188c7
LucasRibP <44446242+LucasRibP@users.noreply.github.com>
2021-07-07 17:46:22 -0300

    Completed strike animation

--- components/Todo.js
+++ components/Todo.js
@@ -28,10 +28,48 @@ export default Todo = ({ todo, onPressTodo }) => {
   };
 
   const onTextLayout = (tl) => {
-    setLineSizes(tl.nativeEvent.lines.map((line) => line.width));
+    setLineSizes(
+      tl.nativeEvent.lines.map((line) => {
+        return { height: line.height, width: line.width };
+      })
+    );
   };
 
-  const createStrikes = () => {};
+  const createStrikes = () => {
+    return lineSizes.map((line, i) => {
+      if (line.width != 0)
+        return (
+          <View
+            key={i}
+            style={{
+              width: line.width,
+              height: line.height,
+            }}
+          >
+            <Animated.View
+              style={{
+                borderRadius: line.height,
+                height: line.height,
+                width: line.width + 10,
+                backgroundColor: "black",
+                transform: [
+                  { scaleY: 0.1 },
+                  {
+                    translateX: checkAnim.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [-(line.width + 10) / 2, 0],
+                    }),
+                  },
+                  { translateX: -5 },
+                  { scaleX: checkAnim },
+                ],
+              }}
+            />
+          </View>
+        );
+      else return <View key={i} />;
+    });
+  };
 
   return (
     <Pressable onPress={onPress}>
@@ -67,7 +105,6 @@ const styles = StyleSheet.create({
   strikesContainer: {
     position: "absolute",
     flex: 1,
-    backgroundColor: "orangered",
   },
 });
 
