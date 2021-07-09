a4599240c44102459553dc1993d34827aa31a8a0
LucasRibP <44446242+LucasRibP@users.noreply.github.com>
2021-07-09 14:43:28 -0300

    Added todo persistance

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StatusBar } from "react-native";
 import AddTodo from "./components/AddTodo";
 import Empty from "./components/Empty";
 import styled from "styled-components";
 import TodoList from "./components/TodoList";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const saveTodos = async () => {
+      try {
+        const jsonValue = JSON.stringify(todos);
+        console.log(todos);
+        await AsyncStorage.setItem("@storage_Key", jsonValue);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    saveTodos();
+  }, [todos]);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("@storage_Key");
+        const value = jsonValue != null ? JSON.parse(jsonValue) : [];
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    loadTodos();
+  }, []);
 
   const addTodo = (todo) => {
     let newTodos = [...todos, todo];
