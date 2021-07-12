fbfa1de49075577eda32ced83e20ecf6e33161c0
LucasRibP <44446242+LucasRibP@users.noreply.github.com>
2021-07-11 22:39:38 -0300

    Created deletion popup file

---
+++ components/DeletionPopUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default DeletionPopUp = (deleteTodo) => {
+  return (
+    <View style={styles.componentContainer}>
+      <View style={styles.popUpBox}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  componentContainer: {},
+  popUpBox: {},
+});
